1dd33dbfdddbb4b76189863c587dc96e30e8132e
NielsGercama <nielsgercama@gmail.com>
2025-01-27 14:56:31 +0100

    change mappings

--- nielsgercama/map.js
+++ nielsgercama/map.js
@@ -1,13 +1,13 @@
 const PAGES = {
-	20 : {
+	2 : {
         type:"miro", //video, miro, 3d
         mirolink:"https://miro.com/app/embed/uXjVKE-Zstg=/?pres=1&frameId=3458764614711847583&embedId=503370172447&autoplay=yep"
 	},
-    30 : {
+    3 : {
         type:"video", //video, miro, 3d
         mirolink:""
     },
-    2 : {
+    4 : {
         type:"3d", //video, miro, 3d
         mirolink:""
     }
