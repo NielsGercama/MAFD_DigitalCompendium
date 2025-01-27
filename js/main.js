ba5221d17752d5ea92946a64b6aeca0ac55bc3bd
NielsGercama <nielsgercama@gmail.com>
2025-01-27 11:42:23 +0100

    Whyyyy

--- js/main.js
+++ js/main.js
@@ -21,8 +21,6 @@ const PDFStart = nameRoute => {
                 canvas.style.pointerEvents = "all";
                 
                 pdfDoc.getPage(numPage).then(page => {
-                    console.log(canvas.width);
-                    console.log(page.getViewport(1.0).width)
                     let viewport = page.getViewport({ scale: canvas.width / 2 * page.getViewport(1.0).width });
                         canvas.height = viewport.height;
                         canvas.width = viewport.width;
